2041631fac8422b71ec3c443b6ce8007ba5500ad
Dylan Barber <dylanbarber1@outlook.com>
2020-06-15 23:09:50 -0400

    Finish leaderboard

--- src/components/Leaderboard.jsx
+++ src/components/Leaderboard.jsx
@@ -13,57 +13,65 @@ export default class Leaderboard extends Component {
         }
     }
 
-    componentDidMount() {
-        this.fetchGridData();
+    async componentDidMount() {
+        const deals = await this.fetchDeals();
+
+        this.totalDealCounts(deals)
     }
 
-    async fetchGridData() {
+    async fetchDeals() {
         // Usually the key would be stored in a .env file but since this is just a mock API site I'll leave it here :)
         const fetchData = await fetch('https://my.api.mockaroo.com/deals?key=5e718740');
-        const parsedData = await fetchData.json();
-        this.setState({ gridData: parsedData });
-
-        this.totalDealCounts();
+        return fetchData.json();
     }
 
-    totalDealCounts() {
+    totalDealCounts(deals) {
+        const totalsMap = {};
 
-        const userMap = {};
+        deals.forEach(deal => {
+            const key = `${deal.creator.firstName} ${deal.creator.lastName}`;
 
-        const gridData = [];
+            // If the user does not exist yet in the map as a key, add them.
+            if (!totalsMap[key]) {
+                totalsMap[key] = {
+                    won: 0,
+                    lost: 0,
+                    open: 0
+                };
+            }
 
-        this.state.gridData.forEach(deal => {
-            if (userMap[`${deal.creator.firstName} ${deal.creator.lastName}`]) {
+            // Tick up the counter for each type of deal that an employee has
+            switch (deal.deal.status) {
+                case 'won':
+                    totalsMap[key].won += 1
+                    break;
 
-                switch (deal.status) {
-                    case 'won':
-                        userMap[`${deal.creator.firstName} ${deal.creator.lastName}`].won += 1
-                        break;
+                case 'lost':
+                    totalsMap[key].lost += 1
+                    break;
 
-                    case 'lost':
-                        userMap[`${deal.creator.firstName} ${deal.creator.lastName}`].lost += 1
-                        break;
+                case 'open':
+                    totalsMap[key].open += 1
+                    break;
 
-                    case 'open':
-                        userMap[`${deal.creator.firstName} ${deal.creator.lastName}`].open += 1
-                        break;
-
-                    default:
-                        throw new Error('Deal type is not "won", "lost", or "open". (In Leaderboard.jsx (render) gridDate.forEach');
-                }
+                default:
+                    throw new Error(`Deal type is not "won", "lost", or "open". (In Leaderboard.jsx (render) parsedData.forEach. value passed was "${deal.status}"`);
             }
         })
 
-        Object.keys(userMap).forEach(employee => {
-            gridData.push({
-                name: employee,
-                won: employee.won,
-                lost: employee.lost,
-                open: employee.open
-            })
-        })
+        //Mapping over the totalsMap to form the data as needed, sorted as well
+        const formedData = Object.keys(totalsMap).map(key => {
+            const { won, lost, open } = totalsMap[key]
 
-        this.setState({gridData})
+            return {
+                name: key,
+                won,
+                lost,
+                open
+            }
+        }).sort((a, b) => b.open - a.open);
+
+        this.setState({ gridData: formedData })
     }
 
     render() {
@@ -71,14 +79,27 @@ export default class Leaderboard extends Component {
         const gridItems = this.state.gridData.map(gridItem => {
             return <LeaderboardItem
                 name={gridItem.name}
+                won={gridItem.won}
+                lost={gridItem.lost}
+                open={gridItem.open}
             />
         })
 
         return (
 
-            <div className={styles.leaderBoardContainer}>
-                {gridItems}
-            </div>
+            <table className={styles.leaderboardTable}>
+                <thead>
+                    <tr>
+                        <th><h2>Name</h2></th>
+                        <th><h2>Won</h2></th>
+                        <th><h2>Lost</h2></th>
+                        <th><h2>Open</h2></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {gridItems}
+                </tbody>
+            </table>
         )
     }
 }
--- src/components/LeaderboardItem.jsx
+++ src/components/LeaderboardItem.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import styles from './LeaderboardItem.module.scss'
+import styles from './Leaderboard.module.scss'
 
 
 export default class LeaderboardItem extends Component {
@@ -7,15 +7,23 @@ export default class LeaderboardItem extends Component {
 
     render() {
         return (
+            <tr>
+                <td>
+                    <h2 className={styles.leaderBoardItemText}>{this.props.name}</h2>
+                </td>
 
-        
-        <div className={styles.leaderBoardItemContainer}>
+                <td>
+                    <h2 className={styles.leaderBoardItemText}>{this.props.won}</h2>
+                </td>
 
-            <div className={styles.columnContainer}>
-                <h2 className={styles.leaderBoardItemText}>{this.props.name}</h2>
-            </div>
-            
-        </div>
+                <td>
+                    <h2 className={styles.leaderBoardItemText}>{this.props.lost}</h2>
+                </td>
+
+                <td>
+                    <h2 className={styles.leaderBoardItemText}>{this.props.open}</h2>
+                </td>
+            </tr>
         )
     }
 }
